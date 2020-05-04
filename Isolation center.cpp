eb69cec438ea09d63e4b7cb8346165c30ed1365e
Aman5613122 <47296280+Aman5613122@users.noreply.github.com>
2020-05-04 17:35:48 +0530

    Create Isolation center.cpp

---
+++ Isolation center.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+
+using namespace std;
+
+int solve(int center, string s,int n)
+{
+    char alpha[]={'a','b','c','d','e','f','g','h','i','j','k','l','m'
+                    ,'n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    int co=0,ans=0;
+    for(int j=0;j<26;j++)
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(alpha[j] == s[i])
+            {
+                co++;
+            }
+        }
+        if(co > center)
+        {
+            ans+=co-center;
+        }
+        co=0;
+    }
+
+    return ans;
+}
+
+int main()
+{
+   int t;
+   cin >> t;
+
+   while(t--)
+   {
+       int n,q;
+       cin >> n >> q;
+       string s;
+        cin >> s;
+       while(q--)
+       {
+           int i;
+           cin >> i;
+
+           cout << solve(i,s,n) <<endl;
+       }
+
+   }
+    return 0;
+}
